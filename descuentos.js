4364ecdc1682f284df817be15787ab0dd1407ab2
Franco Saavedra <francosaavedra@hotmail.com>
2022-03-16 15:39:41 -0500

    Clase 4 - Promedios y mediana

--- descuentos.js
+++ descuentos.js
@@ -1,11 +1,20 @@
-// const precioOriginal = 140;
-// const descuento = 20;
-
 function calcularPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
-
     return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {    
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerHTML = "El precio con Descuento es $" + precioConDescuento;
 
 }
 

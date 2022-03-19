2b3ae03ea347438046a3e9fcd8dc6f81fc6a7662
Franco Saavedra <francosaavedra@hotmail.com>
2022-03-18 19:56:58 -0500

    moda con botones

--- moda.js
+++ moda.js
@@ -16,23 +16,53 @@ const lista1 = [
     1,
 ]
 
+function agregarValores(){
+    const input = document.getElementById("InputArray");
+    const valueTxt = input.value;
+    const value = Number(valueTxt);
+
+    lista1.push(value);    
+    
+    alert(lista1);
+}
+
 const lista1Count = {};
 
-lista1.map(
-    function (elemento) {
-        if (lista1Count[elemento]) {
-            lista1Count[elemento] += 1;
-        } else{
-            lista1Count[elemento] = 1;
-        }        
-    }
-);
+function calculaPromedio(lista){
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+
+        }
+    );
+
+    const promedioLista = sumaLista / lista.length;
+
+    //return promedioLista;
+    alert("esta es el promedio " + promedioLista);
+}
 
 
-const lista1Array = Object.entries(lista1Count).sort(
-    function (elementoA, elementoB) {
-        return elementoA[1] - elementoB[1];
-    }
-);
+function calculaModa(){
+    lista1.map(
+        function (elemento) {
+            if (lista1Count[elemento]) {
+                lista1Count[elemento] += 1;
+            } else{
+                lista1Count[elemento] = 1;
+            }        
+        }
+    );
 
-const moda = lista1Array[lista1Array.length - 1];
+    const lista1Array = Object.entries(lista1Count).sort(
+        function (elementoA, elementoB) {
+            return elementoA[1] - elementoB[1];
+        }
+    );
+    
+    const moda = lista1Array[lista1Array.length - 1];
+
+    alert(moda);
+}
+
+

9e0e904cb3d5704a5d20127ae1528d7ddd36333d
Franco Saavedra <francosaavedra@hotmail.com>
2022-03-22 20:18:34 -0500

    encontrar la moda en un array

--- moda.js
+++ moda.js
@@ -1,19 +1,16 @@
 const lista1 = [
-    1,
-    2,
-    3,
-    4,
-    4,
-    2,
-    3,
-    3,
-    3,
-    3,
-    3,
-    3,
-    2,
-    2,
-    1,
+    100,
+    200,
+    300,
+    400,
+    200,
+    200,
+    300,
+    100,
+    100,
+    100,
+    100,
+    100,
 ]
 
 function agregarValores(){
@@ -26,7 +23,9 @@ function agregarValores(){
     alert(lista1);
 }
 
-const lista1Count = {};
+
+
+
 
 function calculaPromedio(lista){
     const sumaLista = lista.reduce(
@@ -37,14 +36,51 @@ function calculaPromedio(lista){
     );
 
     const promedioLista = sumaLista / lista.length;
+    const promedioFinal = parseInt(promedioLista)
 
     //return promedioLista;
-    alert("esta es el promedio " + promedioLista);
+    alert("esta es el promedio " + promedioFinal);
 }
 
 
-function calculaModa(){
-    lista1.map(
+
+
+function calculaMedia(lista){
+    const mitadLista1 = parseInt(lista.length / 2);
+
+    function esPar(numerito){
+        if (numerito % 2 === 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    let mediana;
+
+    if (esPar(lista.length)){
+        const elemento1 = lista[mitadLista1 - 1];
+        const elemento2 = lista[mitadLista1];
+
+        const promedioElemento1y2 = calculaPromedio([elemento1, elemento2]);
+
+        mediana = promedioElemento1y2;
+    } else{
+        mediana = lista[mitadLista1];
+    }
+
+    alert("esta es la media " + mediana);
+}
+
+
+
+
+
+
+function calculaModa(lista){
+    const lista1Count = {};
+
+    lista.map(
         function (elemento) {
             if (lista1Count[elemento]) {
                 lista1Count[elemento] += 1;
@@ -62,7 +98,7 @@ function calculaModa(){
     
     const moda = lista1Array[lista1Array.length - 1];
 
-    alert(moda);
+    alert("esta es la moda " + moda);    
 }
 
 
